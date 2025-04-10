d59c312c9d882ba3052e7855e5c61131085713f1
unknown <mutiaratyas29@gmail.com>
2025-04-10 09:57:46 +0700

    Praktikum2

---
+++ Praktikum2Kamis10April.m
@@ -0,0 +1,19 @@
+clc
+clear
+
+%Menghitung luas persegi panjang
+p = 10
+l = 20
+lp= p*l
+disp(lp)
+%print %sama kayak disp
+% /n %untuk enter
+
+%Penjumlahan 
+lp1= p+l
+lp2= p-1
+lp3= p/l
+
+disp(lp1)
+disp(lp2)
+disp(lp3)
